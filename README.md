d806d1d28cf124ed9574d14ba04f892f8e9349f2
Felipe Gasper <felipe@felipegasper.com>
2019-04-20 21:02:04 -0400

    beef up POD

--- README.md
+++ README.md
@@ -2,10 +2,32 @@
 
 CBOR::Free - Fast CBOR for everyone
 
+# SYNOPSIS
+
+    $cbor = CBOR::Free::encode( $scalar_or_ar_or_hr );
+
+    # There’s no decoder yet … sorry.
+
 # DESCRIPTION
 
 This library implements CBOR via XS under a license that permits
 commercial usage.
+
+# STATUS
+
+This distribution is an experimental effort.
+
+Note that this distribution’s interface can still change. If you decide
+to use CBOR::Free in your project, please always check the changelog before
+upgrading.
+
+# AUTHOR
+
+[Gasper Software Consulting](http://gaspersoftware.com) (FELIPE)
+
+# LICENSE
+
+This code is licensed under the same license as Perl itself.
 
 # SEE ALSO
 
--- lib/CBOR/Free.pm
+++ lib/CBOR/Free.pm
@@ -9,10 +9,32 @@ use warnings;
 
 CBOR::Free - Fast CBOR for everyone
 
+=head1 SYNOPSIS
+
+    $cbor = CBOR::Free::encode( $scalar_or_ar_or_hr );
+
+    # There’s no decoder yet … sorry.
+
 =head1 DESCRIPTION
 
 This library implements CBOR via XS under a license that permits
 commercial usage.
+
+=head1 STATUS
+
+This distribution is an experimental effort.
+
+Note that this distribution’s interface can still change. If you decide
+to use CBOR::Free in your project, please always check the changelog before
+upgrading.
+
+=head1 AUTHOR
+
+L<Gasper Software Consulting|http://gaspersoftware.com> (FELIPE)
+
+=head1 LICENSE
+
+This code is licensed under the same license as Perl itself.
 
 =head1 SEE ALSO
 
