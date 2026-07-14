577057a0c16d62d36461d517f6514a00d391923b
Felipe Gasper <felipe@felipegasper.com>
2026-07-13 22:40:59 -0400

    more fprintf

--- cbor_free_decode.c
+++ cbor_free_decode.c
@@ -438,6 +438,7 @@ bool _decode_str( pTHX_ decode_ctx* decstate, union numbuf_or_sv* string_u ) {
     }
 
     string_u->numbuf.num.uv = _parse_for_uint_len2( aTHX_ decstate );
+    fprintf(stderr, "after _parse_for_uint_len2\n");
     _RETURN_IF_SET_INCOMPLETE(decstate, false);
 
     _RETURN_IF_INCOMPLETE( decstate, string_u->numbuf.num.uv, false );
